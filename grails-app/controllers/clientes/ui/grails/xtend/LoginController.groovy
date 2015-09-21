8607475bf90d244cae31ae8988526ee856cde91e
Barbani <Barbani@Barbani-PC>
2015-09-21 01:10:36 -0300

    Agregue el inicio de los usuarios

--- grails-app/controllers/clientes/ui/grails/xtend/LoginController.groovy
+++ grails-app/controllers/clientes/ui/grails/xtend/LoginController.groovy
@@ -1,9 +1,11 @@
 package clientes.ui.grails.xtend
 
 import ar.algo.adriba.appModel.LoginAppModel
+import ar.algo.adriba.appModel.UsuariosObjectSet
+import ar.algo.adriba.tp1.Usuario
 
 class LoginController {
-	
+		
 	static scope = "session"
 	
 	static allowedMethods = [
@@ -12,6 +14,7 @@ class LoginController {
 	
 	def calculadora = new LoginAppModel()
 	
+	
 	def index() { 
 		[calculadora: calculadora]
 	}
@@ -19,8 +22,14 @@ class LoginController {
 	def sumar() {
 		calculadora.nombreUsuarioABuscar = new String(params.numero1)
 		calculadora.contrasenia = new String(params.numero2)
+		this.iniciar()
 		calculadora.autorizarLogin()
 		[calculadora: calculadora]
 	}
 
+	def iniciar(){
+		//Usuario usuario = RecetasObjectSet.INSTANCE.crearUsuario
+		UsuariosObjectSet.INSTANCE.crearUsuarios()
+		//RecetasObjectSet.INSTANCE.crearRecetas(usuario)
+	}
 }
